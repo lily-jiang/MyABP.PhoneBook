b3c5cbcbeebe2415ab623dab9255ef95bd018134
lily.jiang <lily.jiang@qedge.cn>
2020-09-01 15:15:20 +0800

    2020-09-01

--- aspnet-core/src/MyABP.PhoneBook.Core/PhoneBooks/Persons/Person.cs
+++ aspnet-core/src/MyABP.PhoneBook.Core/PhoneBooks/Persons/Person.cs
@@ -2,25 +2,34 @@
 using Abp.Domain.Entities.Auditing;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace MyABP.PhoneBook.PhoneBooks.Persons
 {
+    /// <summary>
+    /// 人员
+    /// </summary>
     public class Person : FullAuditedEntity
     {
         /// <summary>
         /// 姓名
         /// </summary>
+       [Required]
+       [MaxLength(50)]
         public string Name { get; set; }
 
         /// <summary>
         /// 邮箱
         /// </summary>
+       [EmailAddress]
         public string Email { get; set; }
 
         /// <summary>
         /// 地址信息
         /// </summary>
+       [MaxLength(200)]
         public string Address { get; set; }
     }
 }
--- aspnet-core/src/MyABP.PhoneBook.Core/PhoneBooks/PhoneNumbers/PhoneNumber.cs
+++ aspnet-core/src/MyABP.PhoneBook.Core/PhoneBooks/PhoneNumbers/PhoneNumber.cs
@@ -1,15 +1,24 @@
 ﻿using Abp.Domain.Entities.Auditing;
+using MyABP.PhoneBook.PhoneBooks.Persons;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace MyABP.PhoneBook.PhoneBooks.PhoneNumbers
 {
     public class PhoneNumber : IHasCreationTime
     {
+        [Required]
+        [Phone]
+        [MaxLength(11)]
         public string Number { get; set; }
 
         public PhoneNumberType Type { get; set; }
+
+        public int PersonId { get; set; }
+
+        public Person Person { get; set; }
 
         public DateTime CreationTime { get; set; }
     }
